fe71f224462f96323067190634a965b349bb96ab
Igor <des.yogi@gmail.com>
2024-12-27 21:38:25 +0200

    ready for testing

--- src/blocks/sp-offers-slider/sp-offers-slider.js
+++ src/blocks/sp-offers-slider/sp-offers-slider.js
@@ -1,4 +1,30 @@
-// document.addEventListener('DOMContentLoaded', function(){});
-// (function(){
-// код
-// }());
+(function(){
+  spOffer = new Swiper('.sp-offers-slider__container', {
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    //loop: true,
+    //initialSlide: 0,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 16
+      },
+      // 960: {
+      //   slidesPerView: 3,
+      //   spaceBetween: 16
+      // },
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 18
+      }
+    }
+  });
+}());
